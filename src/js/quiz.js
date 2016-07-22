1f30ccb7cba5c1a43d59749410e54b45c6048bc2
Alex Horton <alexhortonmusic@gmail.com>
2016-07-22 16:25:30 -0500

    functioning app, still need to do testing suite

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -14,6 +14,10 @@ let player1;
 let player2;
 let player1Box = $('#player1Box');
 let player2Box = $('#player2Box');
+let playByPlay = $('.playByPlay');
+// remaining health of each player
+let remainingHealth1;
+let remainingHealth2;
 
 let botArr = [
 	Megachine,
@@ -66,7 +70,7 @@ let instaBot = (name, chosenBot, box) => {
 	return robot;
 }
 
-function setupFight () {
+$('#startFightBtn').click(function () {
 	let player1Name = $('#player1').val(),
 			player1Bot = $('#player1Select').val(),
 			player2Name = $('#player2').val(),
@@ -80,30 +84,45 @@ function setupFight () {
 	player1 = instaBot(player1Name, player1Bot, player1Box);
 	player2 = instaBot(player2Name, player2Bot, player2Box);
 
-	player1Box.append(`<h2 class="health">${player1.health}</h2>`);
-	player2Box.append(`<h2 class="health">${player2.health}</h2>`);
+	player1Box.append(`<h2 class="health1">${player1.health}</h2>`);
+	player2Box.append(`<h2 class="health2">${player2.health}</h2>`);
 	$('.gameStartContainer').empty();
 	$('.gamePlayContainer').show();
 	console.log('Fight started');
-	beginRound();
+});
+
+
+
+let player1Round = function () {
+	remainingHealth1 = player1.health - player2.attackDamage;
+	player1.health = remainingHealth1;
+	console.log('player1.health', player1.health);
+	playByPlay.prepend(`<h3 class="colorCommentary">${player2.name} used ${player2.attack} against ${player1.name}! Ouch!</h3>`);
+	$('.health1').html(player1.health);
 };
 
-$('#startFightBtn').click(setupFight);
+let player2Round = function () {
+	remainingHealth2 = player2.health - player1.attackDamage;
+	player2.health = remainingHealth2;
+	console.log('player2.health', player2.health);
+	playByPlay.prepend(`<h3 class="colorCommentary">${player1.name} used ${player1.attack} against ${player2.name}! Yowza!</h3>`);
+	$('.health2').html(player2.health);
+};
 
 //player clicks this button for 1 round of damage to each robot
-
-let beginRound = function () {
+$('#attackBtn').click(function () {
 	if (player1.health <= 0) {
 		player1.health = 0;
+		playByPlay.empty();
+		playByPlay.prepend(`<h1>${player1.name} wins!</h1>`);
 		return 'player1';
 	}
 	else if (player2.health <= 0) {
 		player2.health = 0;
+		playByPlay.empty();
+		playByPlay.prepend(`<h1>${player2.name} wins!</h1>`);
 		return 'player2';
 	}
-	console.log('Howdy');
-
-
-};
-
-$('#attackBtn').click(beginRound);
+	player1Round();
+	player2Round();
+});
